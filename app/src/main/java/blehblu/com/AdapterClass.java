920c1d81dd01081595b1cde76fd10eee7f2a02b8
Aryan Ritesh <aryanrtsh@gmail.com>
2021-11-15 22:24:58 +0530

    Linking of activities is done

---
+++ app/src/main/java/blehblu/com/AdapterClass.java
@@ -0,0 +1,80 @@
+package blehblu.com;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.zip.Inflater;
+
+public class AdapterClass extends RecyclerView.Adapter<AdapterClass.CardViewHolder>{
+    private ArrayList<ModelClassWonder> modelList;
+    private Context context;
+
+    public AdapterClass(ArrayList<ModelClassWonder> modelList, Context context){
+        this.modelList= modelList;
+        this.context=context;
+
+    }
+
+    @NonNull
+    @Override
+    public CardViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.card_design,parent,false);
+        return  new CardViewHolder(view);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CardViewHolder holder, int position) {
+    ModelClassWonder model=modelList.get(position);
+    holder.textViewSplash.setText(model.getCategoryName());
+    holder.imgViewSplash.setImageResource(context.getResources()
+            .getIdentifier(model.getImgName(),"drawable",context.getPackageName()));
+
+    holder.cardView.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            if(position==0){
+            Intent intent= new Intent(context,WondersActivity.class);
+            context.startActivity(intent);
+            }
+            else if(position==1){
+                Intent intent= new Intent(context,FermiActivity.class);
+                context.startActivity(intent);
+            }
+
+        }
+    });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return modelList.size();
+    }
+
+    public class CardViewHolder extends RecyclerView.ViewHolder {
+
+        private ImageView imgViewSplash;
+        private TextView textViewSplash;
+        private CardView cardView;
+
+        public CardViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imgViewSplash=itemView.findViewById(R.id.splashWonder);
+            textViewSplash=itemView.findViewById(R.id.textViewSplash);
+            cardView=itemView.findViewById(R.id.cardView);
+
+
+        }
+    }
+}
--- app/src/main/java/blehblu/com/MainActivity.java
+++ app/src/main/java/blehblu/com/MainActivity.java
@@ -8,8 +8,13 @@ import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
+    private ArrayList<ModelClassWonder> arrayList;
+    private AdapterClass adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +22,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         recyclerView=findViewById(R.id.recycler);
         //recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        arrayList=new ArrayList<>();
         recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
         //recyclerView.setLayoutManager(new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL));
+        ModelClassWonder modelClass1 =new ModelClassWonder("wonder","Seven wonders of the world");
+        ModelClassWonder modelClass2= new ModelClassWonder("fermi","The fermi paradox");
+        arrayList.add(modelClass1);
+        arrayList.add(modelClass2);
+        adapter= new AdapterClass(arrayList,this);
+        recyclerView.setAdapter(adapter);
+
+
+
+
 
 
     }
